183e5eff0a5f2943b31242366130c0da21d6ad6e
Dom <dom.mcdonnell@gmail.com>
2017-05-31 13:44:04 +0100

    Step 10 complete - setup database using datamapper to and create first feature test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,15 +3,17 @@ require 'capybara/rspec'
 require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
+require './app/models/link'
+require './app/app'
 
-require File.join(File.dirname(__FILE__), '..', 'app.rb')
+# require File.join(File.dirname(__FILE__), '..', 'app/app.rb')
 ENV['RACK_ENV'] = 'test'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
   # SimpleCov::Formatter::HTMLFormatter
-  ])
+])
 SimpleCov.start
 
 Capybara.app = BookmarkManager
